f55ae235c9128cf4526dadc14b7ea8f4b5afcea1
JRamonCarralero <grukduz@gmail.com>
2025-04-26 00:10:04 +0200

    info

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -5,9 +5,21 @@
 </script>
 
 <template>
-  <ul>
-    <li v-for="project in props.projects" :key="project._id">
-      {{ project.name }}
+  <ul class="projects-list">
+    <li class="project-element" v-for="project in props.projects" :key="project._id">
+      <div class="project-info">
+        <h3 class="project-name">{{ project.name }}</h3>
+        <div class="project-data">
+          <span>Client: {{ project.client }}</span>
+          <span>Initial Date: {{ project.initialDate }}</span>
+          <span>Final Date: {{ project.finalDate }}</span>
+          <span>Status: {{ project.status }}</span>
+          <span>Priority: {{ project.priority }}</span>
+        </div>
+        <div class="project-description">
+          <p>{{ project.description }}</p>
+        </div>
+      </div>
     </li>
   </ul>
 </template>
