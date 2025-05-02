9279430a07edd6d7c1baba5828096224227d9532
JRamonCarralero <grukduz@gmail.com>
2025-05-02 11:19:59 +0200

    aaa

--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -1,9 +1,29 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import { getAPIData } from '@/utils/utils';
 
+const API_PORT = location.port ? `:3333` : ''
+
+const projects = ref([]);
+
+onMounted(async () => {
+  projects.value = await getProjects();
+});
+
+async function getProjects() {
+  const response = await getAPIData(`${location.protocol}//${location.hostname}${API_PORT}/read/projects`);
+  return response;
+}
 </script>
 
 <template>
   <div class="view-title-container">
     <h2 class="view-title">Tareas</h2>
+  </div>
+  <div class="view-filter-container">
+    <label for="select-project">Proyectos:</label>
+    <select id="select-project">
+      <option v-for="project in projects" :key="project._id" :value="project._id">{{ project.name }}</option>
+    </select>
   </div>
 </template>
