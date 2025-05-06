b1f691a5e761828a2af13794129e5a0545d08f71
JRamonCarralero <grukduz@gmail.com>
2025-05-06 12:19:35 +0200

    obtencion de proyectos y usuarios

--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -9,6 +9,9 @@ const authStore = useAuthStore();
 const user = ref(null);
 
 const projects = ref([]);
+const selectedProject = ref(null);
+const usersAssigned = ref([]);
+const usersNotAssigned = ref([]);
 
 onMounted(async () => {
   projects.value = await getProjects();
@@ -19,6 +22,26 @@ async function getProjects() {
   const response = await getAPIData(`${location.protocol}//${location.hostname}${API_PORT}/read/projects`);
   return response;
 }
+
+function getProjectById(id) {
+  return projects.value.find(project => project._id === id);
+}
+
+async function checkAssigned() {
+  const allUsers = await getAPIData(`${location.protocol}//${location.hostname}${API_PORT}/read/users`);
+  const currentProject = getProjectById(selectedProject.value);
+  console.log('currentProject', currentProject);
+  if (currentProject) {
+    console.log('selectedProject', currentProject);
+    allUsers.forEach(user => {
+      if (currentProject.assignedTo.includes(user._id)) {
+        usersAssigned.value.push(user);
+      } else {
+        usersNotAssigned.value.push(user);
+      }
+    })
+  }
+}
 </script>
 
 <template>
@@ -27,8 +50,31 @@ async function getProjects() {
   </div>
   <div class="view-filter-container">
     <label for="select-project">Proyectos:</label>
-    <select id="select-project">
+    <select id="select-project" v-model="selectedProject">
       <option v-for="project in projects" :key="project._id" :value="project._id">{{ project.name }}</option>
     </select>
+    <button id="btn-filter" @click="checkAssigned()">Filtrar</button>
+  </div>
+  <div class="view-task-container">
+    <div class="view-assigned-container">
+      <h3 class="view-assigned-title">Asignadas</h3>
+      <div class="view-assigned-list-container">
+        <select id="select-assigned">
+          <option v-for="user in usersNotAssigned" :key="user._id" :value="user._id">{{ user.name }}</option>
+        </select>
+        <button id="btn-assign">Asignar</button>
+        <ul id="assigned-list">
+          <li v-for="user in usersAssigned" :key="user._id" :value="user._id">{{ user.name }}</li>
+        </ul>
+      </div>
+    </div>
+    <div class="view-container">
+      <div class="view-form-container">
+        <!--<TaskForm />-->
+      </div>
+      <div class="view-list-container">
+        <!--<TaskList />-->
+      </div>
+    </div>
   </div>
 </template>
