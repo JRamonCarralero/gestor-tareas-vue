b65e04218adb9c5912926d58452e3834797f4c00
JRamonCarralero <grukduz@gmail.com>
2025-05-09 12:59:44 +0200

    crear componente sin funcionalidad

---
+++ src/components/TaskList.vue
@@ -0,0 +1,11 @@
+<script setup>
+  const props = defineProps(['tasks'])
+</script>
+
+<template>
+  <ul>
+    <li v-for="task in props.tasks" :key="task._id">
+      {{ task.name }}
+    </li>
+  </ul>
+</template>
