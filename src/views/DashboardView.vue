31403c1ec4fe419453cf40738b57f473e4ffb279
JRamonCarralero <grukduz@gmail.com>
2025-05-12 12:01:08 +0200

    lista

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -8,6 +8,8 @@ const API_PORT = location.port ? `:3333` : ''
 const authStore = useAuthStore()
 const user = ref(null)
 
+const projects = ref([])
+
 onMounted(async () => {
   user.value = authStore.user
   await getProjects()
@@ -16,9 +18,19 @@ onMounted(async () => {
 async function getProjects() {
   const response = await getAPIData(`${location.protocol}//${location.hostname}${API_PORT}/filter/projects`, 'POST', JSON.stringify({ assignedTo: user.value._id }));
   console.log(response)
+  if (response.message !== 'OK') {
+    alert(response.message)
+    return
+  }
+  projects.value = response.data
 }
 </script>
 
 <template>
   <h2>Dashboard</h2>
+  <ul>
+    <li v-for="project in projects" :key="project._id">
+      {{ project.name }}
+    </li>
+  </ul>
 </template>
